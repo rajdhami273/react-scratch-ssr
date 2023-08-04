c733069f4489e86bef2c85172dec5ea307f5e489
Rajendra Dhami <rajendra@Apollo-Rajendra-Dhamis-Mac.local>
2023-08-04 19:21:26 +0530

    From scratch

---
+++ client/App.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function App() {
+  return <>"this is app right no"</>;
+}
---
+++ webpack.config.client.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./client/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    publicPath: "/",
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(s(a|c)ss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg|jpg|png)$/,
+        use: "url-loader",
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: "tsconfig.client.json",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, "client", "components"),
+    },
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+  devServer: {
+    static: "./dist",
+    hot: true,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+  mode: "development",
+};
